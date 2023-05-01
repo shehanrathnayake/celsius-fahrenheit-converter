dd854487c1f44a47d4af7479ec13d1673cbe48a9
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-05-02 04:57:49 +0530

    Corrected spelling errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,56 @@
-const celsius = document.querySelector(".celcius")
-const farenheit = document.querySelector(".farenheit")
-const farenToCel = document.querySelector("#farenToCel")
-const celToFaren = document.querySelector("#celToFaren")
+const celsius = document.querySelector(".celsius")
+const fahrenheit = document.querySelector(".fahrenheit")
+const fahrenToCel = document.querySelector("#fahrenToCel")
+const celToFahren = document.querySelector("#celToFahren")
 
 //For changing the HTML elements order
 const temperature = document.querySelector(".temperature")
 const celElement = temperature.querySelector(".celInput")
 const equal = temperature.querySelector(".equal")
-const farenElement = temperature.querySelector(".farenInput")
+const fahrenElement = temperature.querySelector(".fahrenInput")
 
 //Listening to farenheitToCelcius button
-farenToCel.addEventListener("click", ()=>{
+fahrenToCel.addEventListener("click", ()=>{
 
     //changing the .show and .remove classes
-    farenToCel.classList.add("hide")
-    farenToCel.classList.remove("show")
-    celToFaren.classList.add("show")
-    celToFaren.classList.remove("hide")
+    fahrenToCel.classList.add("hide")
+    fahrenToCel.classList.remove("show")
+    celToFahren.classList.add("show")
+    celToFahren.classList.remove("hide")
 
     //changing the HTML element order
-    temperature.insertBefore(farenElement, celElement)
+    temperature.insertBefore(fahrenElement, celElement)
     temperature.insertBefore(equal, celElement)
 
     //setting placeholders
-    farenheit.placeholder = 0
+    fahrenheit.placeholder = 0
     celsius.placeholder = -17.78
 
     //changing edit access
     celsius.readOnly = true
-    farenheit.readOnly = false
+    fahrenheit.readOnly = false
 
 })
 
 //Listening to celToFarenheit button
-celToFaren.addEventListener("click", ()=>{
+celToFahren.addEventListener("click", ()=>{
 
     //changing the .show and .remove classes
-    celToFaren.classList.add("hide")
-    celToFaren.classList.remove("show")
-    farenToCel.classList.add("show")
-    farenToCel.classList.remove("hide")
+    celToFahren.classList.add("hide")
+    celToFahren.classList.remove("show")
+    fahrenToCel.classList.add("show")
+    fahrenToCel.classList.remove("hide")
 
     //changing the HTML element order
-    temperature.insertBefore(celElement, farenElement)
-    temperature.insertBefore(equal, farenElement)
+    temperature.insertBefore(celElement, fahrenElement)
+    temperature.insertBefore(equal, fahrenElement)
 
     //setting placeholders
     celsius.placeholder = 0
-    farenheit.placeholder = 32
+    fahrenheit.placeholder = 32
 
     //changing edit access
-    farenheit.readOnly = true
+    fahrenheit.readOnly = true
     celsius.readOnly = false
 })
 
@@ -58,19 +58,19 @@ celToFaren.addEventListener("click", ()=>{
 celsius.addEventListener("change", ()=>{
 
     //setting equations and values to farenheit input
-    let value = (celcius.value * 9 / 5) + 32
+    let value = (celsius.value * 9 / 5) + 32
     if (Math.floor(value) === value) {
-        farenheit.value = value
+        fahrenheit.value = value
     } else {
-        farenheit.value = parseFloat(value.toFixed(2))
+        fahrenheit.value = parseFloat(value.toFixed(2))
     }
 })
 
 //Listening to farenheit value input
-farenheit.addEventListener("change", ()=>{
+fahrenheit.addEventListener("change", ()=>{
 
     //setting equations and values to celcius input
-    let value = (farenheit.value - 32) * 5 / 9
+    let value = (fahrenheit.value - 32) * 5 / 9
     if (Math.floor(value) === value) {
         celsius.value = value
     } else {
